557841387ac07ad6cfaf351895defdf3b4585436
(Jun) <(ddoly0106@gmail.com)>
2024-01-09 16:08:58 +0900

    build: project build

---
+++ src/test/java/mpt/mptproject/MptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mpt.mptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MptProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
